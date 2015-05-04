7da37f9f9234c7a7a1479a1eb264481dd03b04c6
Stephen Young <stephen.young@axisto.com>
2015-05-04 23:47:53 +0100

    Adding capture of pieces

--- src/draughts/core.cljs
+++ src/draughts/core.cljs
@@ -30,7 +30,7 @@
       (+ x (* (- 9 y) 10)))
     cell))
 
-(defn is-intervening-square-active [parent-component [cell-x cell-y :as cell] dragged]
+(defn intervening-square-status [parent-component [cell-x cell-y :as cell] dragged]
   (let [data @(om/get-props parent-component)
         dragged-type (:type dragged)
         direction (if (= dragged-type "white")
@@ -44,28 +44,29 @@
                                    [((:forwards direction) cell-x)
                                     ((:backwards direction) cell-y)]))]
 
-    (if (or (and (:active intervening-square1) (not= (:type intervening-square1) dragged-type))
-            (and (:active intervening-square2) (not= (:type intervening-square2) dragged-type)))
-      true
-      false)))
+    (cond
+		 (and (:active intervening-square1) (not= (:type intervening-square1) dragged-type)) intervening-square1
+     (and (:active intervening-square2) (not= (:type intervening-square2) dragged-type)) intervening-square2
+     :else {:active false :position nil :type nil})))
 
 (defn can-piece-move-here [data dragged parent-component]
   (let [direction (if (= (:type dragged) "white")
                     +
                     -)
         cell (:position @data)
-        piece (:position dragged)]
-    (and (not (:active @data))
-         (cond
-           (valid-direction cell piece direction 1) true
-           (and (valid-direction cell piece direction 2) (is-intervening-square-active parent-component cell dragged)) true
-           :else false))))
+        piece (:position dragged)
+				intervening-square (intervening-square-status parent-component cell dragged)]
+		(cond
+		 (= (:active @data) true) {:can-move false :capture false}
+		 (valid-direction cell piece direction 1) {:can-move true :capture false}
+		 (and (valid-direction cell piece direction 2) (:active intervening-square)) {:can-move true :capture intervening-square}
+		 :else {:can-move false :capture false})))
 
 (defn handle-drag-enter [e state data component]
   (.preventDefault e)
   (let [parent-component (:parent-component state)
         dragged (om/get-state parent-component :dragged)]
-    (if (can-piece-move-here data dragged parent-component)
+    (if (:can-move (can-piece-move-here data dragged parent-component))
       (om/set-state! component :valid-move "valid")
       (if (not= dragged @data)
        (om/set-state! component :valid-move "not-valid")))))
@@ -81,9 +82,10 @@
                    :onDrop (fn [e]
                              (.preventDefault e)
                              (let [parent-component (:parent-component state)
-                                   dragged (om/get-state parent-component :dragged)]
-                               (if (can-piece-move-here data dragged parent-component)
-                                 (put! (:update state) [dragged @data]))
+                                   dragged (om/get-state parent-component :dragged)
+																	 move-data (can-piece-move-here data dragged parent-component)]
+                               (if (:can-move move-data)
+                                 (put! (:update state) [dragged @data (:capture move-data)]))
                                (om/set-state! component :valid-move nil)))}
               (if (:active data)
                 (om/build create-piece data {:init-state
@@ -104,6 +106,7 @@
                                       row
                                       {:init-state state}))))))))
 
+
 (defn table-body [data component]
   (reify
     om/IInitState
@@ -113,7 +116,7 @@
     om/IWillMount
     (will-mount [_]
       (go (loop []
-            (let [[old-loc new-loc] (<! (om/get-state component :update))]
+            (let [[old-loc new-loc captured-piece] (<! (om/get-state component :update))]
               (om/transact! data
                             (fn [old-data]
                               (mapv (fn [square]
@@ -124,6 +127,10 @@
                                           :type (:type old-loc)}
                                        (= square old-loc)
                                         {:position (:position old-loc)
+                                         :active false
+                                         :type nil}
+																			 (= square captured-piece)
+																				{:position (:position captured-piece)
                                          :active false
                                          :type nil}
                                        :else square))
